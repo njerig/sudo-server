be100bef03d7094160be623ae8a9801d9885a71f
Njeri Gachoka <njerig00@gmail.com>
2020-08-03 23:34:01 -0400

    get card

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [http-kit "2.3.0"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [environ "1.2.0"]
+                 [lein-environ "0.4.0"]]
   :main ^:skip-aot sudo-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/sudo_server/core.clj
+++ src/sudo_server/core.clj
@@ -2,14 +2,33 @@
   (:gen-class)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [org.httpkit.server :refer [run-server]]))
+            [org.httpkit.server :refer [run-server]]
+            [org.httpkit.client :as client]
+            [environ.core :refer [env]]))
+
+;; env variables
+(def PRIVACY_API_URL 
+  (env :privacy-api-url))
+(def PRIVACY_API_KEY
+  (env :privacy-api-key))
+
+(def options 
+  {:headers {"Content-Type" "application/json"
+             "Authorization" (str "api-key" PRIVACY_API_URL)}})
+
+(defn get-cards [req]
+  (let [resp @(client/get (str PRIVACY_API_URL "/v1/card") options)]
+    {:status  200
+     :headers {"Content-Type" "application/json"}
+     :body    resp}))
 
 (defroutes all-routes
-  (GET "/" [] "Hello world"))
+  (GET "/" [] "Hello world")
+  (GET "/cards" [] get-cards))
 
 (defn -main
   "App entry point"
   [& args]
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
+  (let [port (Integer/parseInt (or (env :port) "8080"))]
     (run-server all-routes {:port port})
     (println (str "Running server at http://127.0.0.1:" port "/"))))
