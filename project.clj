58a49ed3e577fbf6852675790b50941ca8b08721
Njeri Gachoka <njerig00@gmail.com>
2020-08-08 15:53:09 -0400

    change env vars

--- project.clj
+++ project.clj
@@ -4,11 +4,31 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "1.0.0"]
                  [http-kit "2.3.0"]
                  [compojure "1.6.1"]
                  [environ "1.2.0"]]
+  :plugins [[lein-environ "1.2.0"]
+            [lein-pprint "1.3.2"]]
   :main ^:skip-aot sudo-server.core
   :min-lein-version "2.0.0"
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev [:project/dev :profiles/dev]
+             :test [:project/test :profiles/test]
+             :prod [:project/prod :profiles/prod]
+             ;; only edit :profiles/* in profiles.clj
+             :profiles/dev  {}
+             :profiles/test {}
+             :profiles/prod {}
+
+             :project/dev {:dev true
+                           :api-key ~(System/getenv "PRIVACY_API_KEY_SAND")
+                           :api-url ~(System/getenv "PRIVACY_API_URL_SAND")}
+             :project/test {:test true
+                            :api-key ~(System/getenv "PRIVACY_API_KEY_SAND")
+                            :api-url ~(System/getenv "PRIVACY_API_URL_SAND")}
+             :project/prod {:prod true
+                            :api-key ~(System/getenv "PRIVACY_API_KEY_PROD")
+                            :api-url ~(System/getenv "PRIVACY_API_URL_PROD")}})
--- src/sudo_server/core.clj
+++ src/sudo_server/core.clj
@@ -4,31 +4,45 @@
             [compojure.route :as route]
             [org.httpkit.server :refer [run-server]]
             [org.httpkit.client :as client]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.data.json :as json]))
 
 ;; env variables
 (def PRIVACY_API_URL 
-  (env :privacy-api-url))
+  (env :api-url))
 (def PRIVACY_API_KEY
-  (env :privacy-api-key))
+  (env :api-key))
 
+;; request options
 (def options 
   {:headers {"Content-Type" "application/json"
-             "Authorization" (str "api-key " PRIVACY_API_URL)}})
-
+             "Authorization" (str "api-key " PRIVACY_API_KEY)}})
+;; request handlers
 (defn get-cards [req]
   (let [resp @(client/get (str PRIVACY_API_URL "/v1/card") options)]
     {:status  200
      :headers {"Content-Type" "application/json"}
-     :body    resp}))
+     :body    (json/read-str (get resp :body) :key-fn keyword)}))
 
+;; routing
 (defroutes all-routes
   (GET "/" [] "Hello world")
   (GET "/cards" [] get-cards))
+
+;; system
+;; ref to app server instance for stopping/starting
+(defonce server (atom nil))
+
+(defn stop-server []
+  ;; shut down server, waiting 100ms beforehand
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)
+    (println "Server shutting down...")))
 
 (defn -main
   "App entry point"
   [& args]
   (let [port (Integer/parseInt (or (env :port) "8080"))]
-    (run-server all-routes {:port port})
-    (println (str "Running server at http://127.0.0.1:" port "/"))))
+    (reset! server (run-server #'all-routes {:port port}))
+    (println (str "Running server at http://localhost:" port "/"))))
